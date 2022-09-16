abe482748c31759fc213338b9d92343a6b4fe875
immonroe <immanuelmonroe@gmail.com>
2022-09-16 18:53:20 -0400

    Added basic skeleton for project

--- script.js
+++ script.js
@@ -1,14 +1,71 @@
-const container = document.getElementById("container");
+// const container = document.getElementById("container");
 
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    // cell.innerText = (c + 1); if I wanted to include numbers for each box in the CSS grid
-    container.appendChild(cell).className = "grid-item";
-  };
-};
+// function makeRows(rows, cols) {
+//   container.style.setProperty('--grid-rows', rows);
+//   container.style.setProperty('--grid-cols', cols);
+//   for (c = 0; c < (rows * cols); c++) {
+//     let cell = document.createElement("div");
+//     // cell.innerText = (c + 1); if I wanted to include numbers for each box in the CSS grid
+//     container.appendChild(cell).className = "grid-item";
+//   };
+// };
 
-makeRows(16, 16);
+// makeRows(16, 16);
+
+const gridContainer = document.querySelector('.gridContainer');
+const gridPrompt = document.querySelector('.gridPrompt');
+
+// button to change gridsize and start grid generation 
+const gridButton = document.querySelector('.gridButton');
+gridButton.addEventListener('click', () => {
+    let gridSize = prompt("How many squares wide would you like the grid to be?");
+    // error prompt conditionals
+    while (gridSize > 100 || gridSize < 2 || isNaN(gridSize)) {
+        if (isNaN(gridSize)) {
+            gridSize = prompt("Only numbers are supported\nplease pick a number between 2 and 100");
+        } else if (gridSize === null) { //cancel button
+            break;
+        } else {
+            gridSize = prompt("Please pick a number between 2 and 100");
+        }
+    }
+
+    gridGenerator(gridSize);
+});
+
+function gridGenerator(gridWidth) {
+    // empty if squares are already present
+    removeSquares(gridContainer)
+    // generate squares
+    for (let i = 0; i < (gridWidth * gridWidth); i++) {
+        let sides = 640/gridWidth + "px";
+        const squares = document.createElement('div');
+        squares.classList.add('squares');     
+         //css to change width and height
+         squares.style.width = sides;
+         squares.style.height = sides;
+         // appends the generated divs/squares
+        gridContainer.appendChild(squares);
+    }
+    fillColor();
+}
+
+
+function fillColor() {
+    const squares = document.querySelectorAll('.squares');
+    squares.forEach(squares => {
+        squares.addEventListener('mouseenter', () => {      
+            squares.style.backgroundColor = 'pink';
+        });
+    });
+}
+
+//function to empty existing squares before setting a new grid size
+function removeSquares(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+gridGenerator(16);
