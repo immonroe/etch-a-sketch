ee8f6ea503f26f256d7dbcb9531b590218840e4c
immonroe <immanuelmonroe@gmail.com>
2022-09-28 09:18:57 -0400

    Put code in populateBoard funciton

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // creates 16 x 16 grid
-let board = document.querySelector('.board')
-board.style.gridTemplateColumns = 'repeat(16, 1fr)'
-board.style.gridTemplateRows = 'repeat(16, 1fr)'
-
-// 
-for(let i = 0; i < 256; i++) {
-  let square = document.createElement('div')
-  square.style.backgroundColor = 'blue';
-  board.insertAdjacentElement('beforeend', square);
+function populateBoard(size) {
+  let board = document.querySelector('.board')
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  
+  // 
+  for(let i = 0; i < 256; i++) {
+    let square = document.createElement('div')
+    square.style.backgroundColor = 'blue';
+    board.insertAdjacentElement('beforeend', square);
+  }
 }
+
+populateBoard(16);
