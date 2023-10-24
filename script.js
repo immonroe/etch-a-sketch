5668ea7e3d35b67426a2595d4d28af7d246cc519
immonroe <immanuelmonroe@gmail.com>
2023-10-24 09:46:03 -0400

    Fixed bug where spaces were being highlighted my micro mouse movements

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // global variables
-let color = 'black';
+let color = 'black'
 let click = true;
 
 // creates grid
@@ -13,7 +13,7 @@ function populateBoard(size) {
   let amount = size * size;
   for (let i = 0; i < amount; i++) {
     let square = document.createElement('div');
-    square.addEventListener('pointermove', colorSquare);
+    square.addEventListener('mouseover', colorSquare);
     square.style.backgroundColor = 'white';
     board.insertAdjacentElement('beforeend', square);
   }
@@ -34,17 +34,21 @@ changeSize(16); // defaults to 16x16 on startup
 
 // color selection
 function colorSquare() {
-  if (click || (event.pointerType === 'touch' && event.buttons === 1)) {
+  if (click) {
     if (color === 'rainbow') {
       this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    } else if (color === 'shade') {
-      let currentOpacity = this.getAttribute('data-opacity') || 0;
+    } else if(color === 'shade') {
+      
+      // if current opacity is less than 1, run the following function
+      let currentOpacity = this.getAttribute('data-opacity') || 0; // Grabbed opacity on current square/div or defualted value to zero
       if (currentOpacity < 1) {
-        currentOpacity = parseFloat(currentOpacity) + 0.1;
-        this.setAttribute('data-opacity', currentOpacity);
-        this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity})`;
-      }
-    } else {
+      currentOpacity = parseFloat(currentOpacity) + 0.1;
+      this.setAttribute('data-opacity', currentOpacity); // value came back as string, had to concatonate strings for incrementation by converting to interger using parseFloat (parseInt drop decimal)
+      // console.log(currentOpacity);
+
+      this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity})`; // Setting color for the new value
+    }
+  } else {
       this.style.backgroundColor = color;
     }
   }
@@ -57,26 +61,14 @@ function changeColor(choice) {
 function resetBoard() {
   let board = document.querySelector('.board');
   let squares = board.querySelectorAll('div');
-  squares.forEach((div) => (div.style.backgroundColor = 'white'));
+  squares.forEach((div) => div.style.backgroundColor = 'white');  
 }
 
-document.querySelector('body').addEventListener('click', (e) => {
-  if(e.target.tagName != 'BUTTON')  {
-    click = !click;
-    if (click) {
-        document.querySelector('.mode').textContent = 'Mode: Coloring';
-    } else {
-        document.querySelector('.mode').textContent = 'Mode: Not Coloring';
-    }
-
-    }
-    });
-
-    document.querySelector('.board').addEventListener('pointerdown', function () {
-    click = true;
-    });
-
-    document.querySelector('.board').addEventListener('pointerup', function () {
-    click = false;
-    });
-
+document.querySelector('.board').addEventListener('click', () => {
+  click = !click; // Using boolean statement to toggle click feature
+  if(click) {
+    document.querySelector('.mode').textContent = 'Mode: Coloring';
+  } else {
+    document.querySelector('.mode').textContent = 'Mode: Not Coloring';
+  }
+});
